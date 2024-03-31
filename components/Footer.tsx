ebcd34e2766a48efbb406daaf89bf8f596b9ef04
AMARJEET <amarjeetkumarekma@gmail.com>
2024-03-31 22:21:37 +0530

    fix css

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,29 +3,29 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-800 text-white py-8 px-8 min-w-fit">
-      <div className="container mx-auto flex justify-between items-center max-sm:px-1 max-md:flex-col-reverse">
-        <div className="md:w-2/3 flex justify-between max-md:w-screen max-md:mt-4 max-md:gap-3">
-        <div className="md:w-1/2">
-          <h2 className="text-lg font-bold">COMPANY</h2>
-          <ul className="list-none mt-4">
-            <li className="mb-2">About Us</li>
-            <li className="mb-2">Sustainability</li>
-            <li className="mb-2">Terms of Service</li>
-            <li className="mb-2">Privacy</li>
-          </ul>
+    <footer className="bg-gray-800 text-white py-8 px-8">
+      <div className="container mx-auto flex justify-between items-center max-md:flex-col-reverse">
+        <div className="md:w-2/3 flex justify-between max-md:w-screen max-md:mt-4 max-md:gap-3 max-md:px-4 min-w-[360px]">
+          <div className="md:w-1/2">
+            <h2 className="text-lg font-bold">COMPANY</h2>
+            <ul className="list-none mt-4">
+              <li className="mb-2">About Us</li>
+              <li className="mb-2">Sustainability</li>
+              <li className="mb-2">Terms of Service</li>
+              <li className="mb-2">Privacy</li>
+            </ul>
+          </div>
+          <div className="md:w-1/2">
+            <h2 className="text-lg font-bold">RESOURCES</h2>
+            <ul className="list-none mt-4">
+              <li className="mb-2">Info</li>
+              <li className="mb-2">Blog</li>
+              <li className="mb-2">FAQ</li>
+              <li className="mb-2">Status</li>
+            </ul>
+          </div>
         </div>
-        <div className="md:w-1/2">
-          <h2 className="text-lg font-bold">RESOURCES</h2>
-          <ul className="list-none mt-4">
-            <li className="mb-2">Info</li>
-            <li className="mb-2">Blog</li>
-            <li className="mb-2">FAQ</li>
-            <li className="mb-2">Status</li>
-          </ul>
-        </div>
-        </div>
-        <div className="w-1/3 max-md:w-full max-md:px-1">
+        <div className="w-1/3 max-md:w-screen max-md:px-4 min-w-fit">
           <h2 className="text-lg font-bold mb-4">
             SUBSCRIBE TO EMAIL NEWSLETTER
           </h2>
@@ -33,7 +33,7 @@ const Footer: React.FC = () => {
             <input
               type="email"
               placeholder="Enter your email"
-              className="bg-gray-700 text-white py-1 px-2 mr-2 rounded"
+              className="bg-gray-700 text-white py-1 px-1 mr-2 rounded"
             />
             <button className="bg-blue-500 py-1 px-4 rounded">Subscribe</button>
           </form>
